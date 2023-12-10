53e051aba85056388e92b4498429e2aa94b48ea7
Shashank Raj <shashank1_theone@yahoo.co.in>
2023-12-11 01:14:25 +0400

    generating master data

--- Project/Solution/Task_4_DDL.sql
+++ Project/Solution/Task_4_DDL.sql
@@ -3,7 +3,7 @@
 -- *************************************
 CREATE SCHEMA Deloitte_cs;
 
--- Create Location Table
+-- 1. Create Location Table
 -- *************************************
 CREATE TABLE Deloitte_cs.location (
     location_id VARCHAR(25) PRIMARY KEY,
@@ -14,7 +14,7 @@ CREATE TABLE Deloitte_cs.location (
     region VARCHAR(10)
 );
 
--- Create Customer Table
+-- 2. Create Customer Table
 -- *************************************
 CREATE TABLE Deloitte_cs.customer(
     customer_id varchar(25),
@@ -24,63 +24,38 @@ CREATE TABLE Deloitte_cs.customer(
     PRIMARY KEY (customer_id, location_id),
     FOREIGN KEY (location_id) references Deloitte_cs.location(location_id) 
 );
--- Create Product Table
+
+-- 3. Create Product Table
 -- *************************************
 CREATE TABLE Deloitte_cs.product (
+    P_Date DATE,
     product_id VARCHAR(50) PRIMARY KEY,
     category VARCHAR(20),
     sub_category VARCHAR(50),
-    product_name VARCHAR(255)
+    product_name VARCHAR(100)
 );
 
--- Create Order Table
--- *************************************
-CREATE TABLE Deloitte_cs.order (
-    order_id VARCHAR(255) PRIMARY KEY,
-    order_date DATE,
-    ship_mode VARCHAR(255),
-    customer_id INTEGER REFERENCES Deloitte_cs.customer(customer_id)
-);
-
--- Create OrderDetail Table
--- *************************************
-CREATE TABLE Deloitte_cs.order_detail (
-    row_id SERIAL PRIMARY KEY,
-    order_id VARCHAR(255) REFERENCES Deloitte_cs.order(order_id),
-    product_id INTEGER REFERENCES Deloitte_cs.product(product_id),
-    sales NUMERIC,
-    quantity INTEGER,
-    discount NUMERIC,
-    profit NUMERIC
-);
-
--- Create Shipment Table
+-- 4. Create Shipment Table
 -- *************************************
 CREATE TABLE Deloitte_cs.shipment (
-    shipment_id SERIAL PRIMARY KEY,
-    order_id VARCHAR(255) REFERENCES Deloitte_cs.order(order_id),
-    shipment_dt TIMESTAMP,
-    shipment_mode VARCHAR(255)
+    shipment_id VARCHAR(20) PRIMARY KEY,
+    order_id VARCHAR(25) REFERENCES Deloitte_cs.order(order_id),
+    shipment_date TIMESTAMP,
+    shipment_mode VARCHAR(25)
 );
 
-
--- Product Category 
--- If there was an inventory and price list available, this would make sense. Skipping for now. 
-
--- CREATE TABLE Deloitte_cs.P_CAT(
---     CAT_ID varchar(5),
---     CAT_NAME varchar(50)
--- );
-
--- CREATE TABLE Deloitte_cs.Sub_Cat(
---     S_CAT_ID varchar(5),
---     S_CAT_NAME varchar(50)
--- );
-
--- CREATE TABLE Deloitte_cs.Product_List(
---     CAT_ID varchar(5) REFERENCES Deloitte_cs.P_CAT(CAT_ID),
---     S_CAT_ID varchar(5) REFERENCES Deloitte_cs.Sub_Cat(S_CAT_ID),
---     P_ID varchar(10),
---     p_Name varchar(200)
--- );
-
+-- 5. Create customer purchase Table
+-- *************************************
+CREATE TABLE Deloitte_cs.customer_purchase (
+    customer_id varchar(25),
+    product_id VARCHAR(50) REFERENCES Deloitte_cs.product(product_ID),
+    location_id VARCHAR(25),
+    Order_ID VARCHAR(50) PRIMARY KEY,
+    ShipmentID VARCHAR(25) REFERENCES Deloitte_cs.shipment(shipment_id),
+    Sales NUMERIC,
+    Quantity INT,
+    Discount NUMERIC,
+    Profit NUMERIC,
+    order_Date Date,
+    FOREIGN KEY (customer_id, location_id) references Deloitte_cs.customer(customer_id, location_id)
+);
